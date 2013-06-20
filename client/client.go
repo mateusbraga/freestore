06bec802793445724a41585cd88e1556550cd320
Mateus Braga <mateus.a.braga@gmail.com>
2013-06-20 01:52:57 -0300

    Added connection to doozer to get server address

--- client/client.go
+++ client/client.go
@@ -1,14 +1,65 @@
-package client
+package main
 
+import (
+    "github.com/ha/doozer"
+    "log"
+    "net"
+    "fmt"
+)
+
+//var doozerConn *doozer.Conn
 var currentView interface{}
 
-var servers map[int]
+//var servers map[int]
 
 
 func write(val int) {
 
 }
 
+func main() {
+}
+
 func init() {
+    doozerConn, err := doozer.Dial("localhost:8046")
+    if err != nil { log.Fatal(err)
+    }
+
+    rev, err := doozerConn.Rev()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    files, err := doozerConn.Getdir("/goft/servers", rev, 0, -1)
+	if err != nil {
+        log.Fatal(err)
+	}
+	log.Println(files)
+
+	for _, file := range files {
+        path := fmt.Sprintf("/goft/servers/%v", file)
+        addr, rev, err := doozerConn.Get(path, nil)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Println(string(addr))
+        conn, err := net.Dial("tcp", string(addr))
+        if err != nil {
+            fmt.Println("falhou dial", err)
+            err := doozerConn.Del(path, rev)
+            if err != nil {
+                log.Fatal(err)
+            }
+            continue
+        }
+        log.Println("Did it!")
+        // do something with conn to get currentView
+        err = conn.Close()
+        if err != nil {
+            log.Fatal(err)
+        }
+	}
+
     //get currentView from servers
 }
--- server/server.go
+++ server/server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "log"
-    "net"
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"log"
+	"net"
+
+    "github.com/ha/doozer"
 )
 
 var currentView interface{}
@@ -25,27 +25,62 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
-    var port int
+	//var port int
 
-    if len(os.Args) != 2 {
-        log.Fatal("USAGE: server port")
-    }
-    port, err := strconv.Atoi(os.Args[1])
-    if err != nil {
-        log.Fatal("Invalid port: ", err)
-    }
+	//if len(os.Args) != 2 {
+	//log.Fatal("USAGE: server port")
+	//}
+	//port, err := strconv.Atoi(os.Args[1])
+	//if err != nil {
+	//log.Fatal("Invalid port: ", err)
+	//}
 
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", 0))
 	if err != nil {
-	    log.Fatal(err);
+		log.Fatal(err)
 	}
+
+	log.Println("Using address:", ln.Addr())
+	publishAddr(ln.Addr().String())
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-            log.Println("Failed to accept connection: ", err)
+			log.Println("Failed to accept connection: ", err)
 			continue
 		}
 		go handleConnection(conn)
 	}
+}
+
+func publishAddr(addr string) {
+	doozerConn, err := doozer.Dial("localhost:8046")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    rev, err := doozerConn.Rev()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    files, err := doozerConn.Getdir("/goft/servers/", rev, 0, -1)
+	if err != nil {
+        if err.Error() != "NOENT" {
+            log.Fatal(err)
+        }
+	}
+
+    id := len(files)
+    _, err = doozerConn.Set(fmt.Sprintf("/goft/servers/%d", id), 0,  []byte(addr)) 
+    for err != nil && err.Error() == "REV_MISMATCH" {
+        id++
+        _, err = doozerConn.Set(fmt.Sprintf("/goft/servers/%d", id), 0,  []byte(addr)) 
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    //log.Println(id)
+    //log.Println(newRev)
 }
