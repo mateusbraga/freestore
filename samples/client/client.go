dd918042471b4e322064629ceb69e7947a8e00a6
Mateus Braga <mateus.a.braga@gmail.com>
2013-11-23 06:27:27 -0200

    Sample client now create read-latency files

--- samples/client/client.go
+++ samples/client/client.go
@@ -4,33 +4,65 @@ This is a simple client
 package main
 
 import (
+	"bufio"
+	"crypto/rand"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	"mateusbraga/freestore/client"
 )
 
 func main() {
-	var finalValue interface{}
-	var err error
+	sizes := []int{1, 256, 512, 1024}
 
-	for {
-		startRead := time.Now()
-		finalValue, err = client.Read()
-		endRead := time.Now()
+	for _, size := range sizes {
+		times := make([]time.Duration, 1000)
+
+		data := make([]byte, size)
+
+		n, err := io.ReadFull(rand.Reader, data)
+		if n != len(data) || err != nil {
+			log.Fatalln("error to generate data:", err)
+			return
+		}
+
+		err = client.Write(data)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		startWrite := time.Now()
-		err = client.Write(finalValue.(int) + 1)
-		endWrite := time.Now()
-		if err != nil {
+		for {
+
+			startRead := time.Now()
+			_, err = client.Read()
+			endRead := time.Now()
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			//fmt.Printf("Read %v (%v)-> Write (%v)\n", finalValue, endRead.Sub(startRead), endWrite.Sub(startWrite))
+			times = append(times, endRead.Sub(startRead))
+			time.Sleep(300 * time.Millisecond)
+		}
+		saveTime(times, size)
+	}
+}
+
+func saveTime(times []time.Duration, size int) {
+	file, err := os.Create(fmt.Sprintf("/home/mateus/read-latency-%v"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+
+	for _, t := range times {
+		if _, err := w.Write([]byte(fmt.Sprintf("%v\n", t))); err != nil {
 			log.Fatalln(err)
 		}
-
-		fmt.Printf("Read %v (%v)-> Write (%v)\n", finalValue, endRead.Sub(startRead), endWrite.Sub(startWrite))
-		time.Sleep(1 * time.Second)
 	}
 }
