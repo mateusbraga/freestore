c1d82e2ad56c3a2b6dac02bdb50552d30359770a
Mateus Braga <mateus.a.braga@gmail.com>
2014-01-23 00:09:46 -0200

    Add nTotal to client

--- cmd/freestore_client/client.go
+++ cmd/freestore_client/client.go
@@ -4,14 +4,20 @@ Freestore_client is a sample implementation of a freestore client.
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"math"
 	"time"
 
 	"github.com/mateusbraga/freestore/pkg/client"
 
 	"net/http"
 	_ "net/http/pprof"
+)
+
+var (
+	nTotal = flag.Uint64("n", math.MaxUint64, "number of times to perform a read and write operation")
 )
 
 func main() {
@@ -22,7 +28,7 @@ func main() {
 		log.Println(http.ListenAndServe("localhost:6061", nil))
 	}()
 
-	for {
+	for i := uint64(0); i < *nTotal; i++ {
 		startRead := time.Now()
 		finalValue, err = client.Read()
 		endRead := time.Now()
