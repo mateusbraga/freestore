f1559d0fcd93e7c3b0b51d89cdb378fac05284c4
Mateus Braga <mateus.a.braga@gmail.com>
2014-02-02 19:00:03 -0200

    Revert "Remove printfs"
    This reverts commit 634741ddf4e9fdca94f0d6480e309a18c9f6b4be.

--- pkg/comm/communication.go
+++ pkg/comm/communication.go
@@ -55,6 +55,7 @@ func SendRPCRequest(process view.Process, serviceMethod string, arg interface{},
 		return errors.New(fmt.Sprintf("process %v is unreachable", process))
 	}
 
+	fmt.Printf("send %v to process %v\n", serviceMethod, process)
 	err := commLink.rpcClient.Call(serviceMethod, arg, result)
 	if err != nil {
 		commLink.rpcClient = nil
