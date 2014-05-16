21b05d9b32e8ad361fae1116958056aa7cfe2d8b
Mateus Braga <mateus.a.braga@gmail.com>
2014-05-16 11:36:45 -0300

    Fix startReconfiguration

--- pkg/server/reconfiguration.go
+++ pkg/server/reconfiguration.go
@@ -53,7 +53,7 @@ func resetTimerLoop() {
 }
 
 func startReconfiguration() {
-	if shouldDoReconfiguration() {
+	if !shouldDoReconfiguration() {
 		// restart reconfiguration timer
 		resetReconfigurationTimer <- true
 		return
